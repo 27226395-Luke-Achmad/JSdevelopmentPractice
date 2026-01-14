e03af8674666c7722e4771c1c3d5d11b8c086fcb
27226395 <138249289+Luke22King@users.noreply.github.com>
2026-01-14 13:13:29 +0200

    First Commit of My practice committed

---
+++ firstPractice.js
@@ -0,0 +1,7 @@
+let addedString = `I am a phrase`;
+let built = `Concatenation adds the following: `;
+        
+for (let y = 0; y < addedString.length; y++) {
+   built += addedString[y]
+}
+console.log(built);
