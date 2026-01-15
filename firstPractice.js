a821f97bf518985ec00466cfa5c211721cd78be9
27226395 <138249289+Luke22King@users.noreply.github.com>
2026-01-15 11:56:49 +0200

    Adding my latest react app practice and dependency (app using vite)

--- firstPractice.js
+++
@@ -1,7 +0,0 @@
-let addedString = `I am a phrase`;
-let built = `Concatenation adds the following: `;
-        
-for (let y = 0; y < addedString.length; y++) {
-   built += addedString[y]
-}
-console.log(built);
